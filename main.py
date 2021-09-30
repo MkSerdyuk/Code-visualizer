e4b63248c3c90f9f7c76171a10c0006ef4d7b083
UnrealKurd <git_user@mail.ru>
2021-09-30 17:19:07 +0300

    Deep analizer

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 name = 'example.txt'
 f = open(name,'r')
-code = f.read().split('\n').reverse()
+code = f.read().split('\n')
+code.reverse()
 pointers = []
-edited_code
-for i = 1 to len(code):
-    if (code[i].lower()== "end" or "end."):
-        edited_code[i] += " ")
-        pointers += i
-    elif (code[i].lower() == "begin")
-        edited_code[i] += "p_"+pointers(len(pointers))
+edited_code = []
+for i in range(len(code)):
+    if (code[i].lower()== "end" or code[i].lower()=="end."):
+        edited_code.append("->|")
+        pointers.append(str(i))
+    elif (code[i].lower() == "begin"):
+        edited_code.append("->"+pointers[len(pointers)-1])
         del pointers[len(pointers)-1]
-    else
-        edited_code[i] += code[i]
-        
+    else:
+        edited_code.append(code[i])
+edited_code.reverse()
+for i in range(len(edited_code)):
+    print(edited_code[i])
         
     
         
