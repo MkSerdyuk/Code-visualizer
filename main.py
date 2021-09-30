a2fc178de341f270faba00a099061601528a9384
UnrealKurd <git_user@mail.ru>
2021-09-30 16:47:03 +0300

    Code analize

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 name = 'example.txt'
 f = open(name,'r')
-c = f.read().split('\n')
+code = f.read().split('\n').reverse()
+pointers = []
+edited_code
+for i = 1 to len(code):
+    if (code[i].lower()== "end" or "end."):
+        edited_code[i] += " ")
+        pointers += i
+    elif (code[i].lower() == "begin")
+        edited_code[i] += "p_"+pointers(len(pointers))
+        del pointers[len(pointers)-1]
+    else
+        edited_code[i] += code[i]
+        
+        
+    
+        
