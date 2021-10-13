68e48387503fd8465816ba4d781f4ed647e18dc9
UnrealKurd <git_user@mail.ru>
2021-10-13 23:28:50 +0300

    Save Works

--- main.py
+++ main.py
@@ -4,22 +4,65 @@ from tkinter import filedialog
 from tkinter import ttk
 from PIL import Image
 
+start = False
 def start_main():
     file_name = filedialog.askopenfilename()
-    if start == True:
-        result_img.destroy()
-    else:
-        start = True
-    result_img = Label(tab2, image = img)
-    result_img.image = img
-    result_img.pack(relx=.5, rely=.5, anchor="c")
+    #if start == True:
+     #   result_img.destroy()
+    #else:
+     #   start = True
+#_______        
+    name = file_name
+    f = open(name,'r')
+    code = f.read().split('\n')
+    code.reverse()
+    pointers = ['']
+    edited_code = ['']
+    for i in range(0,len(code)):
+        if ("end" in code[i].lower()):
+            edited_code.append("@|")
+            pointers.append(str(len(code)-int(i)+1))
+        elif ("begin" in code[i].lower()):
+            edited_code.append("@"+pointers[len(pointers)-1])
+            del pointers[len(pointers)-1]
+        else:
+            edited_code.append(code[i])
+    edited_code.reverse()
+    #____________________________
+    weidth = 620
+    height = 877
+    interv = 15
+    space = Image.new("RGB", (weidth,height),(256,256,256))
+    text_print = ImageDraw.Draw(space)
+    k=0
+    tabneed=0
+    for i in range(1,height,interv): 
+        if k<len(edited_code):
+            cur_str=str(edited_code[k])
+            if len(cur_str)>0:
+                if cur_str[0]!="@":
+                    text_print.text((10+tabneed*5,i),edited_code[k],(0,0,0))
+                elif cur_str[1]!="|":
+                    pointer=0
+                    for j in range(len(cur_str)-1):
+                        pointer=pointer*10+int(cur_str[j+1])
+                    text_print.line((2+tabneed*5,i,2+tabneed*5,i+(pointer-k-1)*interv),fill='red',width=2)
+                    text_print.text((10+tabneed*5,i),"BEGIN",(0,0,0))
+                    tabneed+=1
+                elif cur_str[1]=="|":
+                    text_print.text((10+tabneed*5,i),"END",(0,0,0))
+        k+=1  
+#____
+    img = space
+    result_img = tk.Label(tab2)
+   #result_img.pack(relx=.5, rely=.5, anchor="c")
+    result_img.place(x=100,y=100)
     save_button = tk.Button(tab2, text = "Сохранить", width = 25, height = 2, bg ='grey',fg = 'black')
     save_button.place(relx=.5,rely=0.9,anchor="c")
     save_button.config(command=lambda:save_image(img))
 def save_image(img):
     file_name = filedialog.asksaveasfilename(filetypes=(("Image File (PNG)", "*.png"),("Image File (JPEG)", "*.jpeg")))
     img.save(file_name)
-start = False
 window = tk.Tk()
 tab_control = ttk.Notebook(window)
 tab1 = ttk.Frame(tab_control)
@@ -31,48 +74,3 @@ window.title("Визуализатор кода")
 open_button = tk.Button(tab1, text = "Открыть", width =25, height = 2, bg = 'grey',fg = 'black') 
 open_button.place(relx=.5, rely=.5, anchor="s")
 open_button.config(command=lambda:start_main())
-
-#_______________
-
-
-name = 'example.txt'
-f = open(name,'r')
-code = f.read().split('\n')
-code.reverse()
-pointers = ['']
-edited_code = ['']
-for i in range(0,len(code)):
-    if ("end" in code[i].lower()):
-        edited_code.append("@|")
-        pointers.append(str(len(code)-int(i)+1))
-    elif ("begin" in code[i].lower()):
-        edited_code.append("@"+pointers[len(pointers)-1])
-        del pointers[len(pointers)-1]
-    else:
-        edited_code.append(code[i])
-edited_code.reverse()
-#____________________________
-weidth = 620
-height = 877
-interv = 15
-space = Image.new("RGB", (weidth,height),(256,256,256))
-text_print = ImageDraw.Draw(space)
-k=0
-tabneed=0
-for i in range(1,height,interv): 
-    if k<len(edited_code):
-        cur_str=str(edited_code[k])
-        if len(cur_str)>0:
-            if cur_str[0]!="@":
-                text_print.text((10+tabneed*5,i),edited_code[k],(0,0,0))
-            elif cur_str[1]!="|":
-                pointer=0
-                for j in range(len(cur_str)-1):
-                    pointer=pointer*10+int(cur_str[j+1])
-                text_print.line((2+tabneed*5,i,2+tabneed*5,i+(pointer-k-1)*interv),fill='red',width=2)
-                text_print.text((10+tabneed*5,i),"BEGIN",(0,0,0))
-                tabneed+=1
-            elif cur_str[1]=="|":
-                text_print.text((10+tabneed*5,i),"END",(0,0,0))
-    k+=1
-space.show()    
